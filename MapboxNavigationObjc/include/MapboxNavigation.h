671fa390eb6c82b211c234214da76fbd755417d5
QuanDHA <quandha@viettel.com.vn>
2026-06-03 13:42:51 +0700

    Update

--- MapboxNavigationObjc/include/MapboxNavigation.h
+++ MapboxNavigationObjc/include/MapboxNavigation.h
@@ -6,6 +6,5 @@ FOUNDATION_EXPORT double MapboxNavigationVersionNumber;
 //! Project version string for MapboxNavigation.
 FOUNDATION_EXPORT const unsigned char MapboxNavigationVersionString[];
 
-//#import "../MBRouteVoiceController.h"
-#import "MBRouteVoiceController.h"
+#import "../MBRouteVoiceController.h"
 #import <MapLibre/Mapbox.h>
